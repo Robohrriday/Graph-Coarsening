8037dc6ec25cfb1782050167779d93e6bde259f9
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-05-11 17:14:44 +0530

    storage space

--- GCN/run.sh
+++ GCN/run.sh
@@ -10,36 +10,36 @@
 # python train.py --dataset Physics --experiment random --epoch 200 --lr 0.001 --weight_decay 0 --coarsening_ratio 0.5
 
 
-# python train.py --dataset cora --experiment fixed --coarsening_ratio 0
-# python train.py --dataset cora --experiment fixed --coarsening_ratio 0.1
-# python train.py --dataset cora --experiment fixed --coarsening_ratio 0.3
-# python train.py --dataset cora --experiment fixed --coarsening_ratio 0.5
+python train.py --dataset cora --experiment fixed --coarsening_ratio 0
+python train.py --dataset cora --experiment fixed --coarsening_ratio 0.1
+python train.py --dataset cora --experiment fixed --coarsening_ratio 0.3
+python train.py --dataset cora --experiment fixed --coarsening_ratio 0.5
 python train.py --dataset cora --experiment fixed --coarsening_ratio 0.7
 
 
-# python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0
-# python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0.1
-# python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0.3
-# python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0.5
-# python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0.7
+python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0
+python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0.1
+python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0.3
+python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0.5
+python train.py --dataset citeseer --experiment fixed --coarsening_ratio 0.7
 
 
-# python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0
-# python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0.1
-# python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0.3
-# python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0.5
-# python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0.7
+python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0
+python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0.1
+python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0.3
+python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0.5
+python train.py --dataset pubmed --experiment fixed --coarsening_ratio 0.7
 
 
-# python train.py --dataset dblp --experiment fixed --coarsening_ratio 0
-# python train.py --dataset dblp --experiment fixed --coarsening_ratio 0.1
-# python train.py --dataset dblp --experiment fixed --coarsening_ratio 0.3
-# python train.py --dataset dblp --experiment fixed --coarsening_ratio 0.5
-# python train.py --dataset dblp --experiment fixed --coarsening_ratio 0.7
+python train.py --dataset dblp --experiment fixed --coarsening_ratio 0
+python train.py --dataset dblp --experiment fixed --coarsening_ratio 0.1
+python train.py --dataset dblp --experiment fixed --coarsening_ratio 0.3
+python train.py --dataset dblp --experiment fixed --coarsening_ratio 0.5
+python train.py --dataset dblp --experiment fixed --coarsening_ratio 0.7
 
 
-# python train.py --dataset Physics --experiment fixed --coarsening_ratio 0
-# python train.py --dataset Physics --experiment fixed --coarsening_ratio 0.1
-# python train.py --dataset Physics --experiment fixed --coarsening_ratio 0.3
-# python train.py --dataset Physics --experiment fixed --coarsening_ratio 0.5
-# python train.py --dataset Physics --experiment fixed --coarsening_ratio 0.7
+python train.py --dataset Physics --experiment fixed --coarsening_ratio 0
+python train.py --dataset Physics --experiment fixed --coarsening_ratio 0.1
+python train.py --dataset Physics --experiment fixed --coarsening_ratio 0.3
+python train.py --dataset Physics --experiment fixed --coarsening_ratio 0.5
+python train.py --dataset Physics --experiment fixed --coarsening_ratio 0.7
